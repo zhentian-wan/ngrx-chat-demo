fed84e828fd7e416a22d082bd53590cdf1886953
Zhentian Wan <answer881215@gmail.com>
2017-01-26 10:52:39 +0200

    scrollIntoView when the messages change

--- src/app/message-section/message-list/message-list.component.ts
+++ src/app/message-section/message-list/message-list.component.ts
@@ -1,12 +1,32 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, Input, ViewChild, OnChanges, SimpleChanges} from '@angular/core';
+import {last} from 'lodash';
 
 @Component({
   selector: 'message-list',
   templateUrl: './message-list.component.html',
   styleUrls: ['./message-list.component.css']
 })
-export class MessageListComponent {
+export class MessageListComponent implements OnChanges{
 
   @Input() messages;
+
+  @ViewChild('list') list;
+
   constructor() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['messages']) {
+      const previous = changes['messages'].previousValue;
+      const current = changes['messages'].currentValue;
+      if(previous.length < current.length) {
+        setTimeout(() => this.scrollToBottom());
+      }
+    }
+  }
+
+  scrollToBottom(): void {
+    const items = this.list.nativeElement.querySelectorAll('li');
+    const lastItem: any = last(items);
+    lastItem.scrollIntoView();
+  }
 }
