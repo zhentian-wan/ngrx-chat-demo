8cd98950efe26661707681b2223db98262dc4fc6
Zhentian Wan <answer881215@gmail.com>
2017-02-05 17:59:37 +0200

    barrels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
-import {StoreModule, combineReducers} from '@ngrx/store';
+import {StoreModule} from '@ngrx/store';
+import {RouterModule} from "@angular/router";
+import {RouterStoreModule} from "@ngrx/router-store";
+import {EffectsModule} from "@ngrx/effects";
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
-import {storeFreeze} from "ngrx-store-freeze";
 
 import {AppComponent} from './app.component';
 import {UserSelectionModule} from "./user-selection/user-selection.module";
@@ -13,17 +15,19 @@ import {MessageSectionModule} from "./message-section/message-section.module";
 import {ThreadsService} from "./services/threads.service";
 
 import {rootReducers} from './reducers/index';
-import {EffectsModule} from "@ngrx/effects";
-import {LoadUserThreadsEffectService} from "./store/effects/load-user-threads.service";
-import {SaveNewMessageEffectService} from "./store/effects/save-new-message.service";
-import {RefreshMessageListEffectService} from "./store/effects/refresh-message-list.service";
-import {MarkUnreadMessageReadEffectService} from "./store/effects/mark-unread-message-read.service";
+import {
+  LoadUserThreadsEffectService,
+  SaveNewMessageEffectService,
+  RefreshMessageListEffectService,
+  MarkUnreadMessageReadEffectService
+} from './store/index';
+
 import { ErrorMessagesComponent } from './error-messages/error-messages.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import {RouterModule} from "@angular/router";
+
 import {routes} from "./app.routes";
-import {RouterStoreModule} from "@ngrx/router-store";
+
 
 @NgModule({
   declarations: [
---
+++ src/app/store/index.ts
@@ -0,0 +1,4 @@
+export {MarkUnreadMessageReadEffectService} from "./effects/mark-unread-message-read.service";
+export {RefreshMessageListEffectService} from "./effects/refresh-message-list.service";
+export {SaveNewMessageEffectService} from "./effects/save-new-message.service";
+export {LoadUserThreadsEffectService} from "./effects/load-user-threads.service";
